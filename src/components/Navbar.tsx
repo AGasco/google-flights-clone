9ce48cd8180ecb4a2625e68af016b250bd2ea78b
Antonio Gasco <antoniofgasco@gmail.com>
2025-02-03 22:10:13 +0000

    Navbar responsive

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,12 +2,39 @@ import FlightIcon from '@mui/icons-material/Flight';
 import HotelIcon from '@mui/icons-material/Hotel';
 import HouseIcon from '@mui/icons-material/House';
 import LuggageIcon from '@mui/icons-material/Luggage';
+import MenuIcon from '@mui/icons-material/Menu';
 import TravelExploreIcon from '@mui/icons-material/TravelExplore';
-import { AppBar, Avatar, Box, Container, Link, Toolbar } from '@mui/material';
+import {
+  AppBar,
+  Avatar,
+  Box,
+  Container,
+  IconButton,
+  Link,
+  Menu,
+  MenuItem,
+  Toolbar,
+  Typography
+} from '@mui/material';
+import * as React from 'react';
 import googleLogo from '../assets/google.svg';
 import NavLink from './NavLink';
 
+const links = ['Travel', 'Explore', 'Flights', 'Hotels', 'Vacation Rentals'];
+
 const Navbar = () => {
+  const [anchorElNav, setAnchorElNav] = React.useState<null | HTMLElement>(
+    null
+  );
+
+  const handleOpenNavMenu = (event: React.MouseEvent<HTMLElement>) => {
+    setAnchorElNav(event.currentTarget);
+  };
+
+  const handleCloseNavMenu = () => {
+    setAnchorElNav(null);
+  };
+
   return (
     <>
       <AppBar
@@ -19,13 +46,55 @@ const Navbar = () => {
             variant="dense"
             sx={{ justifyContent: 'space-between', height: '3.5rem' }}
           >
+            {/* Mobile Menu */}
             <Box
               sx={{
                 justifyContent: 'space-between',
-                display: 'flex'
+                display: { xs: 'flex', md: 'none' }
               }}
             >
-              <Link href="/">
+              <IconButton
+                size="large"
+                aria-label="account of current user"
+                aria-controls="menu-appbar"
+                aria-haspopup="true"
+                onClick={handleOpenNavMenu}
+              >
+                <MenuIcon />
+              </IconButton>
+              <Menu
+                id="menu-appbar"
+                anchorEl={anchorElNav}
+                anchorOrigin={{
+                  vertical: 'bottom',
+                  horizontal: 'left'
+                }}
+                keepMounted
+                transformOrigin={{
+                  vertical: 'top',
+                  horizontal: 'left'
+                }}
+                open={!!anchorElNav}
+                onClose={handleCloseNavMenu}
+                sx={{ display: { xs: 'block', md: 'none' } }}
+              >
+                {links.map((page) => (
+                  <MenuItem key={page} onClick={handleCloseNavMenu}>
+                    <Typography>{page}</Typography>
+                  </MenuItem>
+                ))}
+              </Menu>
+            </Box>
+
+            {/* Logo, Links & Avatar */}
+            <Box
+              sx={{
+                display: 'flex',
+                justifyContent: { xs: 'space-between', md: 'flex-start' },
+                flexGrow: 1
+              }}
+            >
+              <Link href="/" alignContent="center">
                 <img
                   src={googleLogo}
                   alt="Google Logo"
@@ -34,10 +103,17 @@ const Navbar = () => {
                 />
               </Link>
 
-              <Box sx={{ display: 'flex', justifyContent: 'space-between' }}>
+              <Box
+                sx={{
+                  display: 'flex',
+                  flexGrow: 1,
+                  justifyContent: 'space-between',
+                  marginLeft: '1.5rem'
+                }}
+              >
                 <Box
                   sx={{
-                    display: 'flex',
+                    display: { xs: 'none', md: 'flex' },
                     flexGrow: 1
                   }}
                 >
@@ -96,6 +172,7 @@ const Navbar = () => {
           </Toolbar>
         </Container>
       </AppBar>
+      <Toolbar sx={{ marginBottom: '-0.3rem' }} />
     </>
   );
 };

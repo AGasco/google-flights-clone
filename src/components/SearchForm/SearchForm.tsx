bfc43eabee52428d2aa375ae8c90b4345b009b1e
Antonio F. Gasco <antoniofgasco@gmail.com>
2025-02-04 17:06:01 +0000

    Loading and error handling in SearchForm

--- src/components/SearchForm/SearchForm.tsx
+++ src/components/SearchForm/SearchForm.tsx
@@ -1,6 +1,7 @@
 import { SearchOutlined } from '@mui/icons-material';
-import { Box, Button, Grid2, Paper } from '@mui/material';
+import { Box, Button, Grid2, Paper, Typography } from '@mui/material';
 import { useEffect } from 'react';
+import { BounceLoader } from 'react-spinners';
 import { useFlightContext } from '../../context';
 import { useSearchFlights } from '../../hooks';
 import CabinClassSelect from './CabinClassSelect';
@@ -23,7 +24,7 @@ const SearchForm = () => {
     setFlights
   } = useFlightContext();
 
-  const { flights, loading, searchFlights } = useSearchFlights();
+  const { flights, loading, error, searchFlights } = useSearchFlights();
 
   const handleSearch = () => {
     if (!origin || !destination || !departureDate) return;
@@ -46,58 +47,64 @@ const SearchForm = () => {
   }, [flights, setFlights]);
 
   return (
-    <Paper elevation={3} sx={{ mb: '3rem' }}>
-      <Box p={3}>
-        {/* First Row */}
-        <Grid2 container spacing={2} mb={1.5}>
-          <Grid2 size={{ xs: 4, sm: 3, lg: 2 }}>
-            <TripTypeSelect />
+    <>
+      <Paper elevation={3} sx={{ mb: '3rem' }}>
+        <Box p={3}>
+          {/* First Row */}
+          <Grid2 container spacing={2} mb={1.5}>
+            <Grid2 size={{ xs: 4, sm: 3, lg: 2 }}>
+              <TripTypeSelect />
+            </Grid2>
+            <Grid2 size={{ xs: 2, md: 1 }}>
+              <PassengerSelect />
+            </Grid2>
+            <Grid2 size={{ xs: 3, sm: 2 }}>
+              <CabinClassSelect />
+            </Grid2>
           </Grid2>
-          <Grid2 size={{ xs: 2, md: 1 }}>
-            <PassengerSelect />
+          {/* Second Row */}
+          <Grid2 container spacing={2}>
+            <Grid2 size={{ xs: 12, sm: 6, lg: 3 }}>
+              <LocationInput label="From" value={origin} onChange={setOrigin} />
+            </Grid2>
+            <Grid2 size={{ xs: 12, sm: 6, lg: 3 }}>
+              <LocationInput
+                label="To"
+                value={destination}
+                onChange={setDestination}
+              />
+            </Grid2>
+            <Grid2 size={{ xs: 12, lg: 6 }}>
+              <DateSelector />
+            </Grid2>
           </Grid2>
-          <Grid2 size={{ xs: 3, sm: 2 }}>
-            <CabinClassSelect />
-          </Grid2>
-        </Grid2>
-        {/* Second Row */}
-        <Grid2 container spacing={2}>
-          <Grid2 size={{ xs: 12, sm: 6, lg: 3 }}>
-            <LocationInput label="From" value={origin} onChange={setOrigin} />
-          </Grid2>
-          <Grid2 size={{ xs: 12, sm: 6, lg: 3 }}>
-            <LocationInput
-              label="To"
-              value={destination}
-              onChange={setDestination}
-            />
-          </Grid2>
-          <Grid2 size={{ xs: 12, lg: 6 }}>
-            <DateSelector />
-          </Grid2>
-        </Grid2>
 
-        <Button
-          variant="contained"
-          onClick={handleSearch}
-          disabled={loading}
-          sx={{
-            height: 40,
-            borderRadius: 20,
-            mb: '-4.5rem',
-            backgroundColor: '#1676e5',
-            textTransform: 'none',
-            fontWeight: 600,
-            '&:disabled': {
-              backgroundColor: '#f7f7f7'
-            }
-          }}
-        >
-          <SearchOutlined fontSize="small" sx={{ mr: '0.5rem' }} />
-          Search
-        </Button>
-      </Box>
-    </Paper>
+          <Button
+            variant="contained"
+            onClick={handleSearch}
+            disabled={loading}
+            sx={{
+              height: 40,
+              borderRadius: 20,
+              mb: '-4.5rem',
+              backgroundColor: '#1676e5',
+              textTransform: 'none',
+              fontWeight: 600,
+              '&:disabled': {
+                backgroundColor: '#f7f7f7'
+              }
+            }}
+          >
+            <SearchOutlined fontSize="small" sx={{ mr: '0.5rem' }} />
+            Search
+          </Button>
+        </Box>
+      </Paper>
+      {loading && (
+        <BounceLoader color={'#1676e5'} cssOverride={{ margin: '0 auto' }} />
+      )}
+      {error && <Typography variant="subtitle2">{error}</Typography>}
+    </>
   );
 };
 export default SearchForm;

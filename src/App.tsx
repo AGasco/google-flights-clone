0a945561503f417f852a85522aee31e085db91f4
Antonio F. Gasco <antoniofgasco@gmail.com>
2025-02-04 11:11:36 +0000

    Implemented google flights background image

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,24 @@
+import { Box, Container } from '@mui/material';
 import './App.css';
+import backgroundImage from './assets/background.svg';
 import Navbar from './components/Navbar';
 
 function App() {
   return (
     <>
       <Navbar />
-      <h1>App</h1>
+      <Container disableGutters>
+        <Box
+          component="img"
+          src={backgroundImage}
+          aria-hidden="true"
+          sx={{
+            width: '100%',
+            height: '35%',
+            objectFit: 'cover'
+          }}
+        />
+      </Container>
     </>
   );
 }

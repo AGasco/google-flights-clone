de86b8d26249622ffb0391a62a59657cd68b153b
Antonio F. Gasco <antoniofgasco@gmail.com>
2025-02-04 14:16:38 +0000

    Installed datepicker deps + implemented LocalizationProvider wrapper

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,6 @@
 import { CssBaseline } from '@mui/material';
+import { LocalizationProvider } from '@mui/x-date-pickers';
+import { AdapterDateFns } from '@mui/x-date-pickers/AdapterDateFnsV3';
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
@@ -8,7 +10,9 @@ createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <CssBaseline />
     <FlightProvider>
-      <App />
+      <LocalizationProvider dateAdapter={AdapterDateFns}>
+        <App />
+      </LocalizationProvider>
     </FlightProvider>
   </StrictMode>
 );
